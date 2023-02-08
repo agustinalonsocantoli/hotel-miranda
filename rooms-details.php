256eb37a517058953b40ad6da69dbb19199ca50c
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-02-08 13:32:26 +0100

    Trabajando en Rooms Details

---
+++ rooms-details.php
@@ -0,0 +1,164 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="rooms-details">
+    <div class="rooms-details__title">
+        <h3>THE ULTIMATE LUXURY</h3>
+
+        <h1>Ultimate Room</h1>
+
+        <div class="rooms-details__box">
+            <h4>Home<strong> | </strong><span>Rooms Details</span></h4>
+        </div>
+    </div>
+
+    <div class="details">
+        <h5>Double Bed</h5>
+
+        <h3>Luxury Double Bed</h3>
+
+        <p>$345<span>/Night</span></p>
+    
+        <div class="detail-img">
+            <img src="src/img/imgCount.png" alt="img">
+        </div>
+
+    </div>
+
+    <div class="checks">
+        <h5>Check Availability</h5>
+
+        <label>Check In</label>
+        <input type="date" value="2020-07-14" />
+        <label>Check Out</label>
+        <input type="date" value="2020-07-16" />
+
+        <button>Check Availability</button>
+    </div>
+
+    <div class="text">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error 
+            sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia 
+            consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro 
+            quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam 
+            quaerat voluptatem.
+        </p>
+    </div>
+
+    <div class="amenities">
+        <h3>Amenities</h3>
+
+        <div class="amenities-list">
+            <div>
+                <div class="items">
+                    <img src="src/img/rooms/air.svg" alt="air">
+                    <p>Air conditioner </p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/bf.svg" alt="Breakfast">
+                    <p>Breakfast</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/clean.svg" alt="Cleaning">
+                    <p>Cleaning</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/grocery.svg" alt="Grocery">
+                    <p>Grocery</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/shop.svg" alt="Shop">
+                    <p>Shop near</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/support.svg" alt="Support">
+                    <p>24/7 Online Support</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/seg.svg" alt="Security">
+                    <p>Smart Security</p>
+                </div>
+            </div>
+
+            <div>
+                <div class="items">
+                    <img src="src/img/rooms/wifi.svg" alt="wifi">
+                    <p>High speed WiFi</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/cook.svg" alt="Kitchen">
+                    <p>Kitchen</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/shower.svg" alt="Shower">
+                    <p>Shower</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/bed.svg" alt="bed">
+                    <p>Single bed</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/towel.svg" alt="Towels">
+                    <p>Towels</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/key.svg" alt="key">
+                    <p>Strong Locker</p>
+                </div>
+
+                <div class="items">
+                    <img src="src/img/rooms/team.svg" alt="team">
+                    <p>Expert Team</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="founder">
+        <svg width="150" height="150" viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="75" cy="75" r="75" fill="#D5D5D5"/>
+            <circle cx="127" cy="23" r="21" fill="#BDAC8D" stroke="white" stroke-width="4"/>
+            <path d="M134 19C134 19.2559 133.902 19.5119 133.707 19.7069L125.707 27.7069C125.512 27.9031 125.256 28 125 28C124.744 28 124.488 27.9023 124.293 27.707L120.293 23.707C120.098 23.5125 120 23.2562 120 23C120 22.4288 120.467 22 121 22C121.256 22 121.512 22.0977 121.707 22.293L125 25.5875L132.294 18.2938C132.488 18.0978 132.744 18 133 18C133.534 18 134 18.4281 134 19Z" fill="white"/>
+        </svg>
+
+        <h3>Rosalina D. William</h3>
+        <h5>Founder, Qux Co.</h5>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed 
+            do eiusmod tempor incididunt ut labore et dolore.
+        </p>
+
+    </div>
+
+    <div class="cancelation">
+        <h3>Cancellation</h3>
+
+        <p>
+            Phasellus volutpat neque a tellus venenatis, a euismod augue facilisis. Fusce 
+            ut metus mattis, consequat metus nec, luctus lectus. Pellentesque orci quis 
+            hendrerit sed eu dolor. Cancel up to 14 days to get a full refund.
+        </p>
+    </div>
+
+</div>
+
+<?php include 'includes/footer.php'; ?>
