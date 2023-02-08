20f764bd4f3ce28b1e7726f73399ab75eb0bdeb1
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2023-02-09 00:06:41 +0100

    Se realizó Rooms formato Grid para escritorio, y se incorporo Pagination

--- rooms-grid.php
+++ rooms-grid.php
@@ -11,8 +11,37 @@
         </div>
     </div>
 
-    <div class="view__rooms">
+    <div class="rooms-grid__select">
+        <select>
+            <option selected="true" disabled>Order By</option>
+            <option>Low price</option>
+            <option>High price</option>
+        </select>
+    </div>
 
+    <div class="rooms-page1 view__rooms">
+
+    </div>
+
+    <div class="rooms-page2 view__rooms">
+
+    </div>
+
+    <div class="rooms-page3 view__rooms">
+
+    </div>
+
+    <div class="rooms-page4 view__rooms">
+
+    </div>
+
+    <div class="rooms-grid__pagination">
+        <p class="btn_room-prev">«</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p class="btn_room-next">»</p>
     </div>
 
 </div>
